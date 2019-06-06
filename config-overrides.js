5d4b40ccb6c071d185583d86e1d8c45ce9904814
gsp <gsp@compuzz.com>
2019-06-06 20:59:37 +0200

    first pattern

--- config-overrides.js
+++ config-overrides.js
@@ -1,28 +1,32 @@
 const WorkboxWebpackPlugin = require('workbox-webpack-plugin')
-const HtmlCriticalWebpackPlugin = require("html-critical-webpack-plugin");
+const HtmlCriticalWebpackPlugin = require('html-critical-webpack-plugin')
+
 module.exports = function override(config, env) {
-  config.plugins = config
-    .plugins
-    .map(plugin => {
-      if (plugin.constructor.name === 'GenerateSW') {
-        return new WorkboxWebpackPlugin.InjectManifest({swSrc: './src/sw.js', swDest: 'service-worker.js'})
-      }
-      return plugin
-    })
-  config
-    .plugins
-    .push(new HtmlCriticalWebpackPlugin({
-      base: './build',
-      src: 'index.html',
-      dest: 'index.html',
-      inline: true,
-      minify: true,
-      extract: true,
-      width: 375,
-      height: 565,
-      penthouse: {
-        blockJSRequests: false
-      }
-    }))
+  config.plugins = config.plugins.map(plugin => {
+    if (plugin.constructor.name === 'GenerateSW') {
+      return new WorkboxWebpackPlugin.InjectManifest({
+        swSrc: './src/sw.js',
+        swDest: 'service-worker.js',
+      })
+    }
+    return plugin
+  })
+  if (env === 'production') {
+    config.plugins.push(
+      new HtmlCriticalWebpackPlugin({
+        base: 'build',
+        src: 'index.html',
+        dest: 'index.html',
+        inline: true,
+        minify: true,
+        extract: true,
+        width: 375,
+        height: 565,
+        penthouse: {
+          blockJSRequests: false,
+        },
+      })
+    )
+  }
   return config
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,65 @@
-import logo from './logo.svg';
-import React, { Component } from 'react';
-import styles from './App.module.scss';
+import {
+  BottomNavigation,
+  BottomNavigationAction,
+  Grid,
+} from '@material-ui/core'
+import AppBar from '@material-ui/core/AppBar'
+import cyan from '@material-ui/core/colors/cyan'
+import green from '@material-ui/core/colors/green'
+import Container from '@material-ui/core/Container'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import { createMuiTheme, makeStyles } from '@material-ui/core/styles'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import FavoriteIcon from '@material-ui/icons/Favorite'
+import LocationOnIcon from '@material-ui/icons/LocationOn'
+import RestoreIcon from '@material-ui/icons/Restore'
+import { ThemeProvider, withStyles } from '@material-ui/styles'
+import React, { Component } from 'react'
 
-class App extends Component {
-  render() {
+const theme = createMuiTheme({
+  palette: {
+    primary: cyan,
+    secondary: green,
+  },
+})
+
+const styles = {
+  stickToBottom: {
+    width: '100%',
+    position: 'fixed',
+    bottom: 0,
+  },
+}
+
+class App extends Component<any> {
+  public render() {
+    const { classes } = this.props
     return (
-      <div className={styles.App}>
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo"/>
-          <p>
-            Edit
-            <code>src/App.js</code>
-            and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer">
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      <React.Fragment>
+        <CssBaseline />
+        <ThemeProvider theme={theme}>
+          <AppBar position="sticky">
+            <Toolbar>
+              <Typography variant="h6" color="inherit">
+                Photos
+              </Typography>
+            </Toolbar>
+          </AppBar>
+          <Container maxWidth={false}>
+            <Typography component="div" style={{ height: '100%' }}>
+              TEST TEXT
+            </Typography>
+          </Container>
+          <BottomNavigation className={classes.stickToBottom} showLabels>
+            <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
+            <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
+            <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
+          </BottomNavigation>
+        </ThemeProvider>
+      </React.Fragment>
+    )
   }
 }
 
-export default App;
+export default withStyles(styles as any)(App)
