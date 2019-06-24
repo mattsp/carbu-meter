c7f3e75c2dcef44a986d179fb7daf94f0145352d
SPOTO, Gian Matteo <gm.spoto@evs.com>
2019-06-24 11:20:29 +0200

    add react-window first version

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -1,7 +1,8 @@
 import { createStyles, makeStyles, Theme } from '@material-ui/core';
 import React, { ComponentClass } from 'react';
 import { FixedSizeList, ListChildComponentProps } from 'react-window';
-import Row from './Row/Row';
+import InfiniteLoader from "react-window-infinite-loader";
+import Row, { IProps as IPropsRow } from './Row/Row';
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
@@ -15,20 +16,47 @@ const useStyles = makeStyles((theme: Theme) =>
 );
 
 interface IProps {
-    rowRenderer: (props: ListChildComponentProps) => JSX.Element
+    rowRenderer: (props: IPropsRow) => JSX.Element
 }
 
 const defaultProps: IProps = {
-    rowRenderer: (props: ListChildComponentProps) => <Row {...props}/>
+    rowRenderer: (props: IPropsRow) => <Row {...props} />
 }
-const  VirtualizedList = (props: IProps)=> {
+
+const LOADING = 1;
+const LOADED = 2;
+const itemStatusMap: { [index: number]: number } = {};
+
+const isItemLoaded = (index: number) => !!itemStatusMap[index];
+const loadMoreItems = (startIndex: number, stopIndex: number) => {
+    for (let index = startIndex; index <= stopIndex; index++) {
+        itemStatusMap[index] = LOADING;
+    }
+    return new Promise(resolve =>
+        setTimeout(() => {
+            for (let index = startIndex; index <= stopIndex; index++) {
+                itemStatusMap[index] = LOADED;
+            }
+            resolve();
+        }, 2500)
+    );
+};
+const VirtualizedList = (props: IProps) => {
     const classes = useStyles();
     const propsPrivate: IProps = { ...defaultProps, ...props }
     return (
         <div className={classes.root}>
-            <FixedSizeList height={400} width={360} itemSize={46} itemCount={200}>
-                {propsPrivate.rowRenderer}
-            </FixedSizeList>
+            <InfiniteLoader
+                isItemLoaded={isItemLoaded}
+                itemCount={1000}
+                loadMoreItems={loadMoreItems}
+            >
+                {({ onItemsRendered, ref }: {onItemsRendered: any, ref: any}) => (
+                    <FixedSizeList onItemsRendered={onItemsRendered} ref={ref} height={400} width={360} itemSize={46} itemCount={200}>
+                        {propsPrivate.rowRenderer}
+                    </FixedSizeList>
+                )}
+            </InfiniteLoader>
         </div>
     );
 }
--- src/components/List/Row/Row.tsx
+++ src/components/List/Row/Row.tsx
@@ -1,13 +1,16 @@
 import { ListItem, ListItemText } from '@material-ui/core';
 import React from 'react'
+import Skeleton from 'react-loading-skeleton';
 import { ListChildComponentProps } from 'react-window';
 
-const Row = (props: ListChildComponentProps)=> {
+export interface IProps extends ListChildComponentProps {
+    loading?: boolean
+}
+const Row = (props: IProps) => {
     const { index, style } = props;
-
     return (
         <ListItem button style={style} key={index}>
-            <ListItemText primary={`Item ${index + 1}`} />
+            {props.loading ? (<Skeleton />) : (<ListItemText primary={`Item ${index + 1}`} />)}
         </ListItem>
     );
 }
