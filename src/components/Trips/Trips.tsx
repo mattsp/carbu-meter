9ef31fe1ef6e9568e0c4adca9a660cd134f21c39
Matteo <s>
2019-08-04 22:38:07 +0200

    ignore test

--- src/components/Trips/Trips.tsx
+++ src/components/Trips/Trips.tsx
@@ -1,15 +1,17 @@
 import React, { useEffect } from 'react'
-import { ITrip } from '../../store/trip/types';
+import { ITrip } from '../../store/trip/types'
 import List from '../List/List'
 
 interface IProps {
-    trips: {[key: string] : ITrip},
-    totalTrips: number,
-    fetchTrips: () => void
+  trips: { [key: string]: ITrip }
+  totalTrips: number
+  fetchTrips: () => void
 }
 const Trips = ({ trips, totalTrips, fetchTrips }: IProps) => {
-    useEffect(() => { fetchTrips() }, [fetchTrips])
-    return <List dataSource={trips} itemCount={totalTrips} itemSize={46} />
+  useEffect(() => {
+    fetchTrips()
+  }, [fetchTrips])
+  return <List dataSource={trips} itemCount={totalTrips} itemSize={46} />
 }
 
-export default Trips;
+export default Trips
