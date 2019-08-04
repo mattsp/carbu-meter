31a24360e84845e5d4c16614f057033ad64605e4
Matteo <s>
2019-08-04 22:52:46 +0200

    add Fab button

--- src/components/Trips/Trips.tsx
+++ src/components/Trips/Trips.tsx
@@ -1,3 +1,6 @@
+import Fab from '@material-ui/core/Fab'
+import { makeStyles } from '@material-ui/core/styles'
+import AddIcon from '@material-ui/icons/Add'
 import React, { useEffect } from 'react'
 import { ITrip } from '../../store/trip/types'
 import List from '../List/List'
@@ -7,11 +10,28 @@ interface IProps {
   totalTrips: number
   fetchTrips: () => void
 }
+
+const useStyles = makeStyles(theme => ({
+  fab: {
+    position: 'absolute',
+    bottom: theme.spacing(2),
+    right: theme.spacing(2),
+  },
+}))
+
 const Trips = ({ trips, totalTrips, fetchTrips }: IProps) => {
   useEffect(() => {
     fetchTrips()
   }, [fetchTrips])
-  return <List dataSource={trips} itemCount={totalTrips} itemSize={46} />
+  const classes = useStyles();
+  return (
+    <div>
+      <List dataSource={trips} itemCount={totalTrips} itemSize={46} />
+      <Fab color="primary" aria-label="add" className={classes.fab}>
+        <AddIcon />
+      </Fab>
+    </div>
+  )
 }
 
 export default Trips
