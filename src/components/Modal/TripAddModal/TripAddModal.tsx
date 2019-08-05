7d939402794ed9c1411cb9a257cdf18b92383f1a
SPOTO, Gian Matteo <gm.spoto@evs.com>
2019-08-05 16:35:56 +0200

    first validation form

--- src/components/Modal/TripAddModal/TripAddModal.tsx
+++ src/components/Modal/TripAddModal/TripAddModal.tsx
@@ -4,10 +4,17 @@ import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import TextField from '@material-ui/core/TextField';
-import React from 'react';
-import {IProps as IModalProps} from '../Modal';
+import React, { useState } from 'react';
+import { ITrip } from '../../../store/trip/types';
+import { IProps as IModalProps } from '../Modal';
 
-const TripAddModal = (({modal, open, closeModal}: IModalProps) => {
+const TripAddModal = (({ modal, open, closeModal }: IModalProps) => {
+
+    const [values, setValues] = useState<ITrip>({
+        creationDate: Date.now(),
+        distance: -1,
+        id: '',
+    });
 
     const closeHandler = () => {
         if (modal) {
@@ -15,28 +22,40 @@ const TripAddModal = (({modal, open, closeModal}: IModalProps) => {
         }
     }
 
+    const handleChange = (name: keyof ITrip) => (event: React.ChangeEvent<HTMLInputElement>) => {
+        setValues({ ...values, [name]: event.target.value });
+    };
+
     return (<Dialog open={open} onClose={closeHandler} aria-labelledby="form-dialog-title">
         <DialogTitle id="form-dialog-title">New Trips</DialogTitle>
         <DialogContent>
-            <TextField
-                autoFocus
-                margin="dense"
-                id="creationDate"
-                label="Creation Date"
-                type="date"
-                InputLabelProps={{
-                    shrink: true,
-                  }}
-                fullWidth
-            />  
-            <TextField
-                autoFocus
-                margin="dense"
-                id="distance"
-                label="Distance"
-                type="number"
-                fullWidth
-            />
+            <form noValidate autoComplete="off">
+                <TextField
+                    autoFocus
+                    margin="dense"
+                    id="creationDate"
+                    label="Creation Date"
+                    type="date"
+                    required
+                    onChange={handleChange('creationDate')}
+                    error= {!values.creationDate}
+                    InputLabelProps={{
+                        shrink: true,
+                    }}
+                    fullWidth
+                />
+                <TextField
+                    autoFocus
+                    margin="dense"
+                    id="distance"
+                    label="Distance"
+                    type="number"
+                    required
+                    error= {!values.distance || Number(values.distance) < 0 }
+                    onChange={handleChange('distance')}
+                    fullWidth
+                />
+            </form>
         </DialogContent>
         <DialogActions>
             <Button onClick={closeHandler} color="primary">
