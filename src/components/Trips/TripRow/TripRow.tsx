cd1847864fc3280d5537995074b9d3d926c2a101
SPOTO, Gian Matteo <gm.spoto@evs.com>
2019-08-08 16:55:35 +0200

    first version shared dateFns

--- src/components/Trips/TripRow/TripRow.tsx
+++ src/components/Trips/TripRow/TripRow.tsx
@@ -1,10 +1,12 @@
+import DateFnsUtils from '@date-io/date-fns';
 import { ListItemText } from '@material-ui/core';
 import IconButton from '@material-ui/core/IconButton';
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 import DeleteIcon from '@material-ui/icons/Delete';
 import EditIcon from '@material-ui/icons/Edit';
-import React, { Fragment } from 'react'
+import React, { Fragment, useLayoutEffect, useState } from 'react'
 import { utcToLocale } from '../../../helper/date-helper';
+import { loadDateFnsLocale } from '../../../i18n/i18n';
 import { ITrip } from '../../../store/trip/types';
 import { IDataSourceItem } from '../../List/List';
 import Row, { IProps as IRowProps } from '../../List/Row/Row';
@@ -14,7 +16,16 @@ interface IProps extends IRowProps {
     editItem: (id: string) => void
 }
 const TripRow = (props: IProps) => {
-    const creationDate = (item: ITrip) => utcToLocale(item.creationDate).toDateString();
+
+    const [dateFns, setDateFns] = useState<DateFnsUtils>(new DateFnsUtils())
+    useLayoutEffect(()=>{
+        loadDateFnsLocale().then((locale:any)=>{
+            dateFns.locale = locale
+            setDateFns(dateFns)
+        })
+
+    }, [dateFns])
+    const creationDate = (item: ITrip) => dateFns.format(utcToLocale(item.creationDate), 'E dd MMM yyyy');
     const deleteClickHandler = (id: string) => {
         props.deleteItem(id);
     }
