c39ea249c01b3f7069580c4689a04b37b3c467c1
Matteo <s>
2019-08-16 19:22:59 +0200

    fix ref

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -13,7 +13,7 @@ admin.initializeApp(functions.config().firebase)
 const tripsDocRef = functions.firestore.document('trips/{tripId}')
 const counterDocRef = functions.firestore.document('counters/trips')
 export const incrementTripsCounter = tripsDocRef.onCreate((event: any) => {
-  const counterRef = event.data.ref.firestore.doc('counters/trips')
+  const counterRef = event.ref.firestore.doc('counters/trips')
 
   counterRef.get().then((documentSnapshot: any) => {
     const currentCount = documentSnapshot.exists
@@ -31,7 +31,7 @@ export const incrementTripsCounter = tripsDocRef.onCreate((event: any) => {
 })
 
 export const decrementTripsCounter = tripsDocRef.onDelete((event: any) => {
-  const counterRef = event.data.ref.firestore.doc('counters/trips')
+  const counterRef = event.ref.firestore.doc('counters/trips')
 
   counterRef.get().then((documentSnapshot: any) => {
     const currentCount = documentSnapshot.exists
@@ -49,9 +49,9 @@ export const decrementTripsCounter = tripsDocRef.onDelete((event: any) => {
 })
 
 export const recountIncomesCount = counterDocRef.onDelete((event: any) => {
-  const incomesRef = event.data.ref.firestore.collection('trips')
-  const counterRef = event.data.ref.firestore.doc('counters/trips')
-  return incomesRef.get().then((querySnapshot: any) => {
+  const tripsRef = event.ref.firestore.collection('trips')
+  const counterRef = event.ref.firestore.doc('counters/trips')
+  return tripsRef.get().then((querySnapshot: any) => {
     counterRef.set({
       count: querySnapshot.docs.length,
     })
